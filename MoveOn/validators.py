3c2defc3d440e1f98dafc26ea15e2cc49289240a
marcinmot93 <marcin.motyka93@gmail.com>
2023-02-21 18:47:08 +0100

    new

--- MoveOn/validators.py
+++ MoveOn/validators.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ValidationError
 from .models import *
 
-def validate_login_exist(param):
-    if not User.objects.filter(param).exists():
-        raise ValidationError('Incorrect username')
+
+def validate_login_exist(username):
+    if not User.objects.filter(username=username).exists():
+        raise ValidationError('Niepoprawna nazwa użytkownika')
